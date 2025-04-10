40fdf7e91df8438fb1d2d98e13151cabab7516af
Joyce Baumann <baumajoy@students.zhaw.ch>
2025-04-10 19:05:41 +0200

    storyboard

--- docs/Wireframes/Storyboard.py
+++ docs/Wireframes/Storyboard.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from PIL import Image
 
-image = Image.open("bild.png")
-st.image(image, caption="Ein tolles Bild!", use_column_width=True)
+image = Image.open("Storyboard.png")
+st.image(image, caption="Storyboard von der App Trink_us!", use_column_width=True)
