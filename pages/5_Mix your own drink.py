a3d26b97ae8b95b24beb3d358ebcfb9c7b743e76
Carmen Hurschler <hurscca1@students.zhaw.ch>
2025-05-12 21:22:57 +0200

    viel spass joyce <3

--- pages/5_Mix your own drink.py
+++ pages/5_Mix your own drink.py
@@ -11,7 +11,7 @@ base = st.selectbox("Wähle deine Basis-Spirituose:", [
     "Rum", "Wodka", "Gin", "Tequila", "Whiskey", "Likör", "Ohne Alkohol"
 ])
 
-st.markdown("---")
+st.divider()
 
 # Zutaten-Auswahl (Standard + Eigene)
 st.subheader("Zutaten auswählen oder eigene hinzufügen")
@@ -43,7 +43,7 @@ with st.form("Eigene Zutat hinzufügen"):
         else:
             st.warning("Bitte gib eine gültige Zutat ein.")
 
-st.markdown("---")
+st.divider()
 
 # Deko
 decoration = st.selectbox("Wähle eine Dekoration:", [
