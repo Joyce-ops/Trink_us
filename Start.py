1214a177129428558d2fe32713014dc861a5f7db
McqulatMiller <millemcq@students.zhaw.ch>
2025-05-21 21:28:27 +0200

    start

--- Start.py
+++ Start.py
@@ -129,7 +129,7 @@ st.markdown("Willkommen bei Trink us. Bei uns findest du zahlreiche Cocktails, d
 # Hinweis zum Alkoholkonsum
 st.info("""
 ##### *❗Hinweis zum Alkoholkonsum:❗*  
-##Diese Cocktailrezepte enthalten alkoholische Zutaten.  
+Diese Cocktailrezepte enthalten alkoholische Zutaten.  
 Wenn du noch nicht volljährig bist, empfehlen wir dir, die Rezepte ohne Alkohol zuzubereiten als leckere Mocktail-Variante!  
 Genieße verantwortungsvoll und altersgerecht. 🍸✨
 """)
